ce050c6dd64dfb4af11662b1f2a64858cb55b085
JonEffingSnow <fasih1996@gmail.com>
2025-05-07 17:36:35 +0200

    refactor

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,30 +4,18 @@ import { useTranslations } from "next-intl";
 import Link from "next/link";
 import dynamic from "next/dynamic";
 
-// Import map component dynamically (no SSR)
-const MapCircle = dynamic(() => import("@/components/MapCircle"), {
-    ssr: false,
-});
+// Dynamically import MapCircles with SSR disabled
+const MapCircles = dynamic(
+    () => import("@/components/MapCircle").then((mod) => mod.MapCircles),
+    { ssr: false }
+);
 
 export default function LandingPage() {
     const t = useTranslations("HomePage");
 
     return (
         <div className="flex min-h-screen flex-col items-center justify-center relative overflow-hidden bg-white">
-            {/* Left Map Circle */}
-            <div className="map-circle-left absolute left-0 top-1/2 -translate-y-1/2">
-                <MapCircle
-                    center={{ lat: 52.3906, lng: 13.0645 }}
-                    markerLabel="Brandenburg"
-                />
-            </div>
-            {/* Right Map Circle */}
-            <div className="map-circle-right absolute right-0 top-1/2 -translate-y-1/2">
-                <MapCircle
-                    center={{ lat: 41.5296, lng: 2.4445 }}
-                    markerLabel="Mataró"
-                />
-            </div>
+            <MapCircles />
 
             {/* Center Content */}
             <div className="z-10 text-center px-16 md:px-24 lg:px-32 text-black max-w-2xl mx-auto">
