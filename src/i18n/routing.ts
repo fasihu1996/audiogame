c93a576b840b0dd983d20f1283b7f973321e2fef
JonEffingSnow <fasih1996@gmail.com>
2025-05-18 19:39:14 +0200

    catalan added as language

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,6 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-    locales: ["en", "de", "es"],
+    locales: ["en", "de", "es", "ct"],
     defaultLocale: "en",
 });
