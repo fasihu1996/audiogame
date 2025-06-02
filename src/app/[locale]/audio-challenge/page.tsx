3674d90aa624aa72f7ae41cd7d3ed43c6e1286dc
fasihu1996 <fasih1996@gmail.com>
2025-06-02 16:14:12 +0200

    top margin added to waveform

--- src/app/[locale]/audio-challenge/page.tsx
+++ src/app/[locale]/audio-challenge/page.tsx
@@ -403,7 +403,7 @@ export default function TimerPage() {
 
             {/* Main game content */}
             <div className="relative flex flex-col items-center justify-center min-h-screen pt-[10vh] text-black z-10">
-                <div className="bg-white p-0 w-full max-w-[320px] mx-auto">
+                <div className="bg-white p-0 w-full max-w-[320px] mx-auto mt-50">
                     <AudioChallengeCore
                         ref={audioComponentRef}
                         audioUrl={currentChallenge.mediaItem.audioUrl}
